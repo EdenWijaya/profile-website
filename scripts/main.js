b35216844601a8f92d6c1a5c6c6ca08af6b58fb4
EdenWijaya <eden.122140187@student.itera.ac.id>
2024-08-22 21:34:24 +0700

    add keyframe

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,26 @@ window.onbeforeunload = () => {
         form.reset();
     }
 }
+
+const texts = ["Project","And", "Experience"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.text').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 2000); // Delay before switching to the next text
+    } else {
+        setTimeout(type, 100);
+    }
+})();
